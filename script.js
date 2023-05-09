b2cca513007144c8b2040449025a459446826886
Nisser111 <kulakmilosz2006@gmail.com>
2023-05-09 19:26:08 +0200

    Empty task list button function

---
+++ script.js
@@ -0,0 +1,39 @@
+// tasks list
+let tasks = [];
+
+// get text from input
+function addNewTask() {
+
+}
+
+// is input empty 
+function isInputEmpty() {
+
+}
+
+// add new task to list
+function addNewTaskToList() {
+
+}
+
+// edit task
+function editTask() {
+
+}
+
+// delete task
+function deleteTask() {
+
+}
+
+// refreshening task list when changed
+function refreshTaskList() {
+
+}
+
+// focus to input when add_task_button clicked
+const addTaskBtn = document.querySelector(".add-task-empty-task-list");
+addTaskBtn.addEventListener("click", () => {
+    const input = document.querySelector(".add_new_task--input");
+    input.focus();
+});
