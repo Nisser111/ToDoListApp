6ce73fae634b4e258c521e8574b06d130b4b2657
Nisser111 <kulakmilosz2006@gmail.com>
2023-05-12 18:55:10 +0200

    adding tasks from template; generate tasks id; hiding empty task info

--- script.js
+++ script.js
@@ -1,52 +1,117 @@
+// selectors
+const addNewTaskInput = document.querySelector(".add_new_task--input");
+const addNewTaskBtn = document.querySelector(".add_new_task--btn");
+const addTaskBtn = document.querySelector(".add-task-empty-task-list");
+const noTaskInfo = document.querySelector("#no-task-info");
+const list = document.querySelector(".tasks-list ul");
+
+
 // tasks list
 let tasks = [];
+let usedIndex = [];
 
-// get text from input
-function addNewTask() {
-    const input = document.querySelector(".add_new_task--input");
-    let v = input.value; 
 
-    if(v) return v;
-    else return false; 
+// auto focus to add new task input
+window.onload = () => {
+    addNewTaskInput.focus()
 }
 
-// is input empty 
-function isInputEmpty() {
+// task constructor
+function Task(v){
+    this.text = v;
+    this.dateOfAdded =  new Date();
+    this.index = this.generateId();
+    this.renderTask = function() {    
+        let newTask = document.createElement("li");
+        newTask.classList.add("task");
     
+        // If browswere support tempalte tag
+        if ("content" in document.createElement("template")) {
+            const template = document.querySelector("#taskTempalte");
+            const clone = template.content.cloneNode(true);
+            
+            const input = clone.querySelector(".task-text");
+            input.value = this.text;
+    
+            newTask.append(clone);
+        }
+    
+        list.append(newTask)
+    };
+
+
 }
 
-// get text from input
-function getText() {
-    const input = document.querySelector(".add_new_task--input");
-    if(!input.value) 
-        return true
-    else
-        return false;
-}
+// create task id
+Task.prototype.generateId = function() {
+    function generate() {
+        return (Math.random() + 1).toString(36).substring(7);
+    }
 
-// add new task to list
-function addNewTaskToList() {
-
+    let r = generate();
+    while(true) {
+        if(usedIndex.includes(r)) 
+            r = generate()
+        else 
+            usedIndex.push(r);
+            return r;
+    }
 }
 
 // edit task
-function editTask() {
+Task.prototype.editTask = function() {
 
 }
 
 // delete task
-function deleteTask() {
+Task.prototype.deleteTask = function(event) {
 
 }
 
-// refreshening task list when changed
-function refreshTaskList() {
+// is input empty 
+function isInputEmpty() {
+    const input = document.querySelector(".add_new_task--input");
+    if(!input.value) 
+        return true;
+    else
+        return false;
+}
 
+// is tasks list empty
+function hideNoTaskInfo() {
+    if(!tasks.length)
+        noTaskInfo.remove();
+}
+
+// create task
+function createTask() {
+    let task = new Task(addNewTaskInput.value);
+    addNewTaskInput.value = "";
+    tasks.push(task);
+    task.renderTask();
 }
 
 // focus to input when add_task_button clicked
-const addTaskBtn = document.querySelector(".add-task-empty-task-list");
 addTaskBtn.addEventListener("click", () => {
     const input = document.querySelector(".add_new_task--input");
     input.focus();
 });
+
+
+// When add new task button was clicked 
+addNewTaskBtn.addEventListener("click", function() {
+    if (isInputEmpty()) {
+        alert("Please, write what you want to do");
+    } else {
+        hideNoTaskInfo();
+        createTask(tasks);
+    }
+});
+
+// When user delete task
+list.addEventListener("click", e => {
+    if(e.target.classList.contains("delete--btn")) {
+        e.target.deleteTask(e);
+    }
+})
+
